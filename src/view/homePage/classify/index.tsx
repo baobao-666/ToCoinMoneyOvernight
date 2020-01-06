1208229a5e2c223fcf28f4916cd8b64dbdaf7c25
159593919 <55084632+159593919@users.noreply.github.com>
2020-01-06 11:41:56 +0800

    date

--- src/view/homePage/classify/index.tsx
+++ src/view/homePage/classify/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import './index.css'
 
 const  ClassFiy: React.FC = () => {
   return (
     <div className="classfiy-wrap">
-      分类
+     
+        <input type="text" placeholder='搜索商品，共239款好物'/>
+      
     </div>
   );
 }
