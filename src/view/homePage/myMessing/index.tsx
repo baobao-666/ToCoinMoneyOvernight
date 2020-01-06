25e446ee17263dd522eb3a23adf295fbe116bc68
NewbieOnTheRoad <55943042+NewbieOnTheRoad@users.noreply.github.com>
2020-01-06 15:50:46 +0800

    my

--- src/view/homePage/myMessing/index.tsx
+++ src/view/homePage/myMessing/index.tsx
@@ -1,10 +1,68 @@
 import React from 'react';
-
+import './css/index.css'
 
 const  My: React.FC = () => {
   return (
     <div className="my-wrap">
-     我的详情
+            <div className="userMsgWrap">
+                <div className="userLogo"></div>
+                <div className="userMsgs">
+                      <div>15323807318</div>
+                      <div>普通用户</div>
+                </div>
+            </div>
+            <div className="userPower">
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+                <div className="item">
+                    <i></i>
+                    <div>我的收藏</div>
+                </div>
+            </div>
+            <div className="loginOut">退出登录</div>
+            
     </div>
   );
 }
