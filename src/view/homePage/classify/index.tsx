6d3a3b7269d789a93ee1c03a267fbfa4a77c1cca
159593919 <55084632+159593919@users.noreply.github.com>
2020-01-06 10:58:12 +0800

    add

--- src/view/homePage/classify/index.tsx
+++ src/view/homePage/classify/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import '../css/index.css'
 
 const  ClassFiy: React.FC = () => {
   return (
     <div className="classfiy-wrap">
-      分类
+      <div className='inp'><input type="text" name="" id="" placeholder='搜索商品，共239款好物'/></div>
     </div>
   );
 }
