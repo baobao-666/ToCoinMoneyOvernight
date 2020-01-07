c0583a225d0ed3a785cab1c8ab298b150c3977f9
NewbieOnTheRoad <55943042+NewbieOnTheRoad@users.noreply.github.com>
2020-01-07 10:53:43 +0800

    `-`

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 import * as Router from "react-router-dom"
-import StoreContext  from  './context/index'
+import StoreContext from './context/index'
 import store from './store/index'
-import 'px2rem-loader/lib/px2rem-loader'
-ReactDOM.render( 
-               <StoreContext.Provider value={store} >
-                    <Router.BrowserRouter> 
-                          <App /> 
-                    </Router.BrowserRouter>
-               </StoreContext.Provider>
-                
-                                         , document.getElementById('root'));
+
+ReactDOM.render(
+      <StoreContext.Provider value={store} >
+            <Router.BrowserRouter>
+                  <App />
+            </Router.BrowserRouter>
+      </StoreContext.Provider>
+
+      , document.getElementById('root'));
