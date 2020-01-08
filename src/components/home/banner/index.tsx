9c9fa74664146f3f948f319d213246e549b7bea2
baobao666 <1084403501@qq.com>
2020-01-08 18:40:00 +0800

    banner

--- src/components/home/banner/index.tsx
+++ src/components/home/banner/index.tsx
@@ -2,36 +2,25 @@ import React  from 'react'
 
 import { Carousel, WingBlank } from 'antd-mobile';
 
-interface Type {
-    Bannerlist:ItemType[]
-}
-
-interface ItemType {
-    id: number,
-    ad_position_id: number,
-    media_type: number,
-    name: string,
-    link: string,
-    image_url:string,
-    content: string,
-    end_time: number,
-    enabled: number
-}
+import { ItemType, Type } from '../../../store/types/home'
 
 const  Banner:React.FC<Type> =(props)=>{
-  return <WingBlank>
+  return <WingBlank 
+         style={{margin:0}}
+         >
            <Carousel
              autoplay={true}
              infinite
-             beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
-             afterChange={index => console.log('slide to', index)}
+             beforeChange={(from, to) =>{}}
+             afterChange={index =>{}}
            >
           {
             props.Bannerlist.map((val:ItemType) => (
                  // eslint-disable-next-line jsx-a11y/anchor-is-valid
                  <a
                   key={val.id}
-                  style={{ display: 'inline-block', width: '100%', height:176 }}
+                  href="#"
+                  style={{ display: 'inline-block', width: '100%' }}
                  >
                    <img
                    src={val.image_url}
--- src/view/homePage/home/index.tsx
+++ src/view/homePage/home/index.tsx
@@ -3,13 +3,29 @@ import { useObserver } from 'mobx-react-lite'
 
 import userStore from '../../../utils/useState'
 
+import style from './css/index.module.css'
+
+// 轮播组件
 import Banner from '../../../components/home/banner/index'
+// 导航组件
+import NavCode from '../../../components/home/navList/index'
+
+// 新商品组价
+import NewListCode from '../../../components/home/newGoodsList/index'
+
+// 制造商组件
+import BrandListCode from '../../../components/home/brandListCode/index'
+
+// 热门商品
+import HotGoodsCode from '../../../components/home/hotGood/index'
+
 
 const  Home: React.FC = () => {
 
   let store = userStore();
   let { Home } = store
   
+
   useEffect(()=>{
     Home.getHomeData()
   },[])
@@ -17,17 +33,12 @@ const  Home: React.FC = () => {
   
   
   return useObserver(()=>( 
-    <div className="home-wrap">
-
-       {
-        // <Banner Bannerlist={ Home.list && Home.list[Home.itemKey[0]]} > </Banner>
-       Home.itemKey.map((item:string,index:number)=>{
-       
-          return  Home.list[item].map((val:any,ind:number)=>{
-              return <div key={ind} > {val.id} </div>
-            })
-       })
-     }
+    <div className={style.home_wrap}> 
+       <Banner Bannerlist = { Home.BannerList }></Banner>
+       <NavCode NavList = { Home.NavList }  ></NavCode>
+       <BrandListCode title={'品牌制造商提供'} brandList = { Home.brandList } ></BrandListCode>
+       <NewListCode title = {'新品首发'} newGoodsList = { Home.newGoodsList }  ></NewListCode>
+       <HotGoodsCode title = {'人气推荐'} hotGoodsList = { Home.hotGoodsList } ></HotGoodsCode>
     </div>
    )
   );
