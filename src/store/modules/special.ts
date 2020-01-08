1409b0ef902b1f7254a1c4066c446f9120ba4240
Your Name <you@example.com>
2020-01-08 21:05:38 +0800

    comment

--- src/store/modules/special.ts
+++ src/store/modules/special.ts
@@ -3,8 +3,9 @@ import {specialType} from '../types/special'
 // 引入mobx
 import {observable,action} from 'mobx'
 import {
-    getSpecialData,
-    getSpecialDetailDate
+    getSpecialData,  // 专题数据
+    getSpecialDetailDate,   // 专题详情
+    getCommentData          // 专题评论
 } from '../../api/special'
 
 
@@ -12,20 +13,34 @@ import {
 export default class CreateStore{
     @observable
     list:specialType[]=[]; // 专题数据
+
+    @observable
     detailData:specialType[]=[]  //详情数据
+
+    @observable
+    commentData:specialType[]=[]  //评论数据
 
     @action
     async getSpecialData(obj:object){
         const res:any =await getSpecialData(obj);
         this.list = res.data;
-        console.log(res.data,'data');
+        console.log(res,'专题数据');
     }
     @action
     async getSpecialDetailDate(obj:Object){
         const res:any =await getSpecialDetailDate(obj);
-        console.log(res);
+        this.detailData=[res]; 
+        console.log(this.detailData,'详情数据');
         
-        // this.detailData = res.data;
     }
+
+    @action
+    async getCommentData(obj:Object){
+        const res:any =await getCommentData(obj);
+        this.commentData=res.data;
+        console.log(res.data,'评论');
+        
+    }
+     
 
 }
