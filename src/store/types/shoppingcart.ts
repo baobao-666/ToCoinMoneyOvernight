740bbbb9caa7e77318d48e1531bb2d02bbf14d7e
159593919 <55084632+159593919@users.noreply.github.com>
2020-01-08 08:58:32 +0800

    shop

--- src/store/types/shoppingcart.ts
+++ src/store/types/shoppingcart.ts
@@ -2,8 +2,6 @@ export interface shopCartType{
     list_pic_url:string,
     goods_name:string,
     retail_price:number,
-    number:number,
-    goodsCount?:number,
-    cartTotal:string,
-    total:Object
+    number:number
+   
 }
