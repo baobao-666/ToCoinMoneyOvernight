3bcedaf7beaaf3e61ed7c0de63e9fe2bca66e4fd
KenKi9 <1595953919@qq.com>
2020-02-15 11:56:13 +0800

    time

---
+++ weixin/pages/home/home.js
@@ -0,0 +1,104 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    year: "2000",
+    mouth: "00",
+    arr: [],
+    mytimes: 0
+  },
+  chagtime: function (event) {
+    var value = event.detail.value;
+    this.setData({
+      year: value.split("-")[0],
+      mouth: value.split("-")[1]
+    });
+    this.settime();
+  },
+  clear: function () {
+    this.setData({
+      year: "",
+      mouth: "",
+      arr: [],
+      mytimes: 0
+    });
+  },
+  onShow: function () {
+    this.settime();
+  },
+  settime: function () {
+    console.log;
+    var _a = this.data, year = _a.year, mouth = _a.mouth;
+    var oldtime = mouth === "00" ? new Date(year).getTime() : new Date(year + "-" + mouth).getTime();
+    var newtime = new Date(new Date().getFullYear() + "-" + (new Date().getMonth() + 1)).getTime();
+    if (newtime > oldtime) {
+      var arr = newtime - oldtime;
+      var sums = Math.ceil(arr / (1000 * 60 * 60 * 24 * 31));
+      this.setData({
+        mytimes: sums
+      });
+    }
+  },
+  
+
+  
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
