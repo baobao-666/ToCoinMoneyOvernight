55289539ff1029116034346113379a824270546f
KenKi9 <1595953919@qq.com>
2020-02-20 19:34:29 +0800

    面试信息

--- sign/src/pages/sign/address/index.tsx
+++ sign/src/pages/sign/address/index.tsx
@@ -1,76 +1,159 @@
 import { ComponentClass } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
+import { View, Text, Input, Button, Form  } from '@tarojs/components'
 import './index.scss'
-import { View, Text, Form, Input, Button } from '@tarojs/components';
-class Address extends Component{
-    config: Config={
-        navigationBarTitleText:'添加面试'
-    }
-    state={
-        company:'',
-        phone:'',
-        time:'',
-        address:'',
-        info:''
-    }
-    componentDidShow(){}
-    componentDidHide(){}
-formSubmit(e){
-    console.log(this.state)
+import { connect } from '@tarojs/redux'
+import {submitSign} from '../../../actions/sign'
+
+
+type PageStateProps = {
+  address: {
+    [key:string]: any
+  },
+  flag: number
 }
-formResrt(){
+
+type PageDispatchProps = {
+  submit: (payload:{})=>void,
+  resetSubmit: ()=>void
+}
+
+type PageOwnProps = {}
+
+type PageState = {}
+
+type Addrops = PageStateProps & PageDispatchProps & PageOwnProps
+
+interface AddSign {
+  props: Addrops;
+}
+const mapStateToProps = state=>{
+  return {
+    address: state.sign.address,
+    flag: state.sign.flag
+  }
+}
+
+const mapDispatchToProps = dispatch=>{
+  return {
+    submit: payload=>{
+      dispatch(submitSign(payload))
+    },
+    resetSubmit: ()=>{
+      dispatch({
+        type: 'SUBMIT_SIGN',
+        payload: -1
+      })
+    }
+  }
+}
+
+const mergeProps = (stateProps, dispatchProps, ownProps)=>{
+  return Object.assign({}, ownProps, stateProps, dispatchProps)
+}
+
+const options = {
+  pure: true  // 默认为true，会对组件执行shouldComonentUpdate的对比
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
+class AddSign extends Component<PageState> {
+  config: Config = {
+    navigationBarTitleText: '添加面试'
+  }
+
+  state={
+    company: '',
+    phone: '',
+    time: '',
+    address: '',
+    info: ''
+  }
+
+  componentDidShow () {
+    console.log('this.props...', this.props.address);
+  }
+
+  componentDidHide () { }
+
+  formSubmit(e){
+    console.log('e...', this.state);
+    this.props.submit({
+      company: this.state.company,
+      phone: this.state.phone,
+      form_id: '',
+      address: this.props.address.addr,
+      latitude: this.props.address.latitude,
+      longitude: this.props.address.longitude,
+      start_time: +new Date(),
+      description: this.state.info
+    })
+  }
+
+  formReset(){
     this.setState({
-        company:'',
-        phone:'',
-        time:'',
-        address:'',
-        info:''
+      company: '',
+      phone: '',
+      address: '',
+      time: '',
+      info: ''
     })
+  }
 
-}
-goLocation(){
+  goLocation(){
     wx.navigateTo({
-        url:'/pages/sign/location/index'
+      url: '/pages/sign/location/index'
     })
-}
-    render(){
-        return(
-           <View className='box'>
-               <View className="mian">面试信息</View>
-               <Form className='text' onSubmit={this.formSubmit.bind(this)} onReset={this.formResrt.bind(this)}>
-                   <View className='address'> 
-                       <Text>公司名称</Text>
-                       <Input  placeholder='请输入公司名称' value={this.state.company} onInput={e=>this.setState({company:e.detail.value})} placeholder-class="placeholderStyle"/>
-                       </View>
-                   <View className='address'> 
-                       <Text>公司电话</Text>
-                       <Input  placeholder='请输入面试联系人电话' value={this.state.phone} onInput={e=>this.setState({phone:e.detail.value})} placeholder-class="placeholderStyle"/>
-                       </View>
-                   <View className='address'> 
-                       <Text>面试时间</Text>
-                       <Input  placeholder='面试时间' value={this.state.time} onInput={e=>this.setState({time:e.detail.value})} placeholder-class="placeholderStyle"/>
-                       </View>
-                   <View className='address'> 
-                       <Text>面试地址</Text>
-                       <Input  placeholder='请选择面试地址' onClick={this.goLocation} placeholder-class="placeholderStyle"/>
-                       </View>
-                       <View className='bei'>
-                          
-                           <Input placeholder='备注信息' value={this.state.info} onInput={e=>this.setState({info:e.detail.value})} placeholder-class="placeholderStyle"/>
-                       </View>
-                       <View className="button">
+  }
 
-                       <Button form-type='submit'>确认</Button>
-                       <Button form-type='reset'>重置</Button>
-                       </View>
-                       
-                 
-
-               </Form>
-              
-
-           </View>
-        )
+  render () {
+    if (this.props.flag === 0){
+      wx.showToast({
+        title: '添加面试失败',
+        icon: 'none'
+      })
+      this.props.resetSubmit();
+    }else if(this.props.flag === 1){
+      wx.showToast({
+        title: '添加面试成功',
+        icon: 'none'
+      })
+      this.props.resetSubmit();
     }
+
+
+    console.log('this.props...', this.props.address, this.props.address.addr);
+    let {addr} = this.props.address;
+    return (
+      <View className='wrap'>
+         <Form onSubmit={this.formSubmit.bind(this)} onReset={this.formReset.bind(this)} className="text">
+          <View className="address">
+            <Text>公司名称</Text>
+            <Input placeholder="公司名称" value={this.state.company} onInput={e=>this.setState({company:e.detail.value})} ></Input>
+          </View>
+          <View className="address">
+            <Text>公司电话</Text>
+            <Input placeholder="公司电话" value={this.state.phone} onInput={e=>this.setState({phone:e.detail.value})}></Input>
+          </View>
+          <View className="address">
+            <Text>面试时间</Text>
+            <Input placeholder="面试时间" value={this.state.time} onInput={e=>this.setState({time:e.detail.value})}></Input>
+          </View>
+          <View className="address">
+            <Text>面试地址</Text>
+            <Text onClick={this.goLocation}>{addr}</Text>
+          </View>
+          <View className="address">
+            <Text>备注</Text>
+            <Input placeholder="备注" value={this.state.info} onInput={e=>this.setState({info:e.detail.value})}></Input>
+          </View>
+          <Button form-type="submit">确认</Button>
+          <Button form-type="reset">重置</Button>
+         </Form>
+      </View>
+    )
+  }
 }
-export default Address as ComponentClass
+
+
+export default AddSign as ComponentClass;
