8b1156e67f6942af1910b7ad0eaee8825811b00c
baobao-666 <1084403501@qq.com>
2020-02-24 10:19:42 +0800

    tab

--- 小程序/sign/src/pages/sign/interviewList/index.tsx
+++ 小程序/sign/src/pages/sign/interviewList/index.tsx
@@ -6,7 +6,8 @@ import { connect } from '@tarojs/redux'
 
 // 接口
 import { getIntervie } from '../../../actions/sign'
-import { View, Text } from '@tarojs/components'
+import { View, Text, ScrollView } from '@tarojs/components'
+import { AtActivityIndicator } from 'taro-ui'
 
 type PageStateProps = {
     list: Array<{
@@ -55,7 +56,7 @@ class Intervie extends Component<{}, PageState> {
         params: {
             status: 2,
             page: 1,
-            pageSize: 10
+            pageSize: 6
         },
         tabList: [{
             title: "未开始",
@@ -69,7 +70,20 @@ class Intervie extends Component<{}, PageState> {
         }, {
             title: "全部",
             id: 4
-        }]
+        }],
+        dargStyle: {//下拉框的样式
+            bottom: 0 + 'px'
+        },
+        updaStyle: {
+            top: 0 + 'px'
+          
+        },
+        downDragStyle: {//下拉图标的样式
+            height: 0 + 'px'
+        },
+        upDragStyle: {//上拉图标样式
+            height: 0 + 'px'
+        }
     }
     componentDidShow() {
         let params = { ... this.state.params }
@@ -79,55 +93,89 @@ class Intervie extends Component<{}, PageState> {
         this.props.getSignList(params)
 
     }
-    setTime = (time):string =>  {
-     
-            let date = new Date(time);
-            console.log(date);
-            
-             //时间戳为10位需*1000，时间戳为13位的话不需乘1000
-            let y = date.getFullYear();
-            let MM:any = date.getMonth() + 1;
-            MM = MM < 10 ? ('0' + MM) : MM;//月补0
-            let d:any = date.getDate();
-            d = d < 10 ? ('0' + d) : d;//天补0
-            let h:any = date.getHours();
-            h = h < 10 ? ('0' + h) : h;//小时补0
-            let m:any = date.getMinutes();
-            m = m < 10 ? ('0' + m) : m;//分钟补0
-            let s:any = date.getSeconds();
-            s = s < 10 ? ('0' + s) : s;//秒补0
-            return y + '-' + MM + '-' + d + ' ' + h + ':' + m+ ':' + s;
-        
+    setTime = (time): string => {
+
+        let date = new Date(time * 1);
+        //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+        let y = date.getFullYear();
+        let MM: any = date.getMonth() + 1;
+        MM = MM < 10 ? ('0' + MM) : MM;//月补0
+        let d: any = date.getDate();
+        d = d < 10 ? ('0' + d) : d;//天补0
+        let h: any = date.getHours();
+        h = h < 10 ? ('0' + h) : h;//小时补0
+        let m: any = date.getMinutes();
+        m = m < 10 ? ('0' + m) : m;//分钟补0
+        let s: any = date.getSeconds();
+        s = s < 10 ? ('0' + s) : s;//秒补0
+        return y + '-' + MM + '-' + d + ' ' + h + ':' + m + ':' + s;
+
+    }
+    onScroll = (e) => {
+        console.log(e.detail)
+    }
+    onScrollToUppers = () => {
+        console.log("上拉时间");
+
+    }
+    onScrollToLowers = () => {
+        console.log("下拉时间");
+
     }
     render() {
-        return (<View className='wrap' >
-            <View className="tabheader" >
-                {
-                    this.state.tabList.map((item, index) => {
-                        return <Text key={index} >{item.title}</Text>
-                    })
-                }
+        const scrollStyle = {
+            height: '100%'
+        }
+        const scrollTop = 0
+        const Threshold = 20
+
+        return (<View className='wrap'>
+            <View className='downDragBox' style={this.state.updaStyle}>
+                <AtActivityIndicator></AtActivityIndicator>
+                <Text className='downText'>上拉刷新</Text>
             </View>
-            <View className='context'>
-                {
-                    this.props.list.length > 0 && this.props.list.map((item, index) => {
-                        return <View className="item" key={index} >
-                            <Text className="item_text" > {item.description}{item.address} </Text>
-                            <Text className="item_text" > {item.company} </Text>
-                            <Text className="item_text" >面试时间:<Text>{this.setTime(item.create_time)}</Text> </Text>
-                            <Text className='mess' >未开始</Text>
-                            <Text className='remind' >未提醒</Text>
-                        </View>
-                    })
-                }
+            <ScrollView
+            className="dragUpdata"
+                scrollY={true}
+                scrollWithAnimation={true}
+                scrollTop={scrollTop}
+                style={scrollStyle}
+                lowerThreshold={Threshold}
+                upperThreshold={Threshold}
+                onScrollToUpper={() => this.onScrollToUppers()} // 使用箭头函数的时候 可以这样写 `onScrollToUpper={this.onScrollToUpper}`
+                onScroll={(e) => this.onScroll(e)}
+                onScrollToLower={() => this.onScrollToLowers()}
+            >
+                <View className="tabheader" >
+                    {
+                        this.state.tabList.map((item, index) => {
+                            return <Text key={index} >{item.title}</Text>
+                        })
+                    }
+                </View>
+                <View className='context'>
+                    {
+                        this.props.list.length > 0 && this.props.list.map((item, index) => {
+                            return <View className="item" key={index} >
+                                <Text className="item_text" > {item.description}</Text>
+                                <Text className="item_text" > {item.company}{item.address} </Text>
+                                <Text className="item_text" >面试时间:<Text>{this.setTime(item.start_time)}</Text> </Text>
+                                <Text className='mess' >未开始</Text>
+                                <Text className='remind' >未提醒</Text>
+                            </View>
+                        })
+                    }
 
+                </View>
+            </ScrollView>
+            <View className='upDragBox' style={this.state.dargStyle}>
+                <AtActivityIndicator></AtActivityIndicator>
+                <Text className='downText'>下拉加载</Text>
             </View>
 
 
-        </View>)
+      </View>  )
     }
-
-
 }
 
 
